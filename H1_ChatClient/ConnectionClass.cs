5d965799c2904182bcc70b81f21a1abcac033140
Tinghus <m.tinghus@gmail.com>
2023-03-24 05:36:35 +0100

    Add project files.

---
+++ H1_ChatClient/ConnectionClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ChatClient
+{
+    public class Connection
+    {
+        public string Ip { get; set; }
+        public int port { get; set; }
+        private TcpClient socketForServer;
+        public bool isConnected = true;
+
+        public NetworkStream networkStream { get; set; }
+        public StreamReader streamReader { get; set; }
+        public StreamWriter streamWriter { get; set; }
+
+        public Connection(string myIp, int port)
+        {
+            this.Ip = myIp;
+            this.port = port;
+        }
+
+        public void ConnectToServer()
+        {
+            try
+            {
+                socketForServer = new TcpClient(Ip.ToString(), port);
+            }
+            catch
+            {
+                Console.WriteLine("Connection failed");
+                return;
+            }
+        }
+
+        public void ServerConnection()
+        {
+            networkStream = socketForServer.GetStream();
+            streamReader = new StreamReader(networkStream);
+            streamWriter = new StreamWriter(networkStream);
+        }
+
+        public void Disconect()
+        {
+            streamWriter.Flush();
+            streamWriter.Close();
+            networkStream.Close();
+            streamWriter.Close();
+            socketForServer.Close();
+        }
+    }
+}
---
+++ H1_ChatClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading;
+
+namespace H1_ChatClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ipAddress = "127.0.0.1";
+            int port = 8080;
+
+            Connection client = new Connection(ipAddress, port);
+
+            client.ConnectToServer();
+            Console.Write("Press any key");
+            Console.ReadKey();
+            Console.Clear();
+
+            client.ServerConnection();
+            try
+            {
+                string messageSent = "";
+                string messageRecieved = "";
+
+                while (client.isConnected)
+                {
+                    messageSent = Console.ReadLine();
+
+                    if (messageSent == "quit")
+                    {
+                        client.isConnected = false;
+                    }
+                    else
+                    {
+                        client.streamWriter.WriteLine(messageSent);
+                        client.streamWriter.Flush();
+                        messageRecieved = client.streamReader.ReadLine();
+                        Console.WriteLine(messageRecieved);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Connection Error");
+                Console.ReadKey();
+            }
+
+            client.Disconect();
+
+        }
+    }
+}
